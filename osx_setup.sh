58493a9d1b5f2f52b2d848fd4c7a457fcbfd168a
Jason Reimer <jareimer1@gmail.com>
2016-02-19 20:19:18 -0600

    added osx setup script

---
+++ osx_setup.sh
@@ -0,0 +1,28 @@
+
+echo 'Install Homebrew'
+
+ruby -e "$(curl -fsSL "https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+echo 'Installing apps'
+
+brew tap caskroom/cask
+brew install caskroom/cask/brew-cask
+brew install wget
+brew install node
+brew install docker
+brew install docker-machine
+brew install android-sdk
+brew install awscli
+brew cask install --appdir="/Applications" caskroom/versions/sublime-text3
+brew cask install --appdir="/Applications" google-chrome
+brew cask install --appdir="/Applications" github
+brew cask install --appdir="/Applications" sublime-text
+brew cask install --appdir="/Applications" virtualbox
+brew cask install --appdir="/Applications" vagrant
+brew cask install --appdir="/Applications" vagrant-manager
+brew cask install --appdir="/Applications" sourcetree
+brew cask install --appdir="/Applicaitons" intellij-idea
+
+docker-machine create --driver virtualbox default
+
+echo 'Installs complete.'
